90b962ae800708180b44ee4c9bc869ede53fb66e
noteve07 <njb.noteve12@gmail.com>
2025-09-13 22:13:51 +0800

    Initialize backend

---
+++ backend/app/models/sensor_data.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+
+
+class SensorData(BaseModel):
+    lat: float
+    lon: float
+    lux: int
+    sensor_name: str
+    timestamp: datetime
+    barangay: Optional[str] = None
+    street: Optional[str] = None
