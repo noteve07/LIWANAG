6b105855a7cb81c8755dda6c3c29d17bf735bf5a
Lee Gabriel Isidro <160717471+gableee@users.noreply.github.com>
2025-09-24 02:14:42 +0800

    Layout

--- frontend/application/src/RouteLayout/RootLayout.tsx
+++ frontend/application/src/RouteLayout/RootLayout.tsx
@@ -6,15 +6,22 @@ export default function RootLayout() {
   const location = useLocation();
   
   return (
-    <div className="flex h-screen bg-[#070B13] overflow-hidden">
-      <div className="fixed h-screen z-50">
-        <Sidebar />
+    <div className="flex bg-[#070B13] overflow-hidden">
+      <div className="fixed w-screen">
+        <Header />
       </div>
       
-      <div className="flex flex-col flex-1 ml-60 h-screen">
-        <Header />
-        <main className="flex-1 bg-gradient-to-br from-blue-900/20 via-transparent to-purple-900/30 overflow-y-auto">
+<<<<<<< Updated upstream
+      <div className="flex flex-1 h-screen pt-17">
+        <Sidebar />
+        <main className="flex-1 bg-gradient-to-br from-yellow-900/30 via-transparent to-yellow-900/30 overflow-y-auto">
+          <div className="h-full">
+=======
+      <div className="flex flex-1 h-screen">
+        <Sidebar />
+        <main className="flex-1 bg-gradient-to-br from-blue-900/20 via-transparent to-purple-900/30 overflow-y-auto pt-14">
           <div className="h-full bg-gradient-to-br from-yellow-900/40 via-transparent to-yellow-900/20">
+>>>>>>> Stashed changes
             <PageTransition key={location.pathname.split('/')[1]}>
               <Outlet />
             </PageTransition>
