5faeec2b60a9d1a288441ee2289df714e76348f9
Lee Gabriel Isidro <160717471+gableee@users.noreply.github.com>
2025-09-24 02:16:07 +0800

    Update RootLayout.tsx

--- frontend/application/src/RouteLayout/RootLayout.tsx
+++ frontend/application/src/RouteLayout/RootLayout.tsx
@@ -11,17 +11,10 @@ export default function RootLayout() {
         <Header />
       </div>
       
-<<<<<<< Updated upstream
       <div className="flex flex-1 h-screen pt-17">
         <Sidebar />
         <main className="flex-1 bg-gradient-to-br from-yellow-900/30 via-transparent to-yellow-900/30 overflow-y-auto">
           <div className="h-full">
-=======
-      <div className="flex flex-1 h-screen">
-        <Sidebar />
-        <main className="flex-1 bg-gradient-to-br from-blue-900/20 via-transparent to-purple-900/30 overflow-y-auto pt-14">
-          <div className="h-full bg-gradient-to-br from-yellow-900/40 via-transparent to-yellow-900/20">
->>>>>>> Stashed changes
             <PageTransition key={location.pathname.split('/')[1]}>
               <Outlet />
             </PageTransition>
